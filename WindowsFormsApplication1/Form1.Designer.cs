cfa33d6319f8109d120a29a92a567f19d7808688
Shakior <ivashkevicha@gmail.com>
2017-02-03 11:35:14 +0300

    добавлены файлы

---
+++ WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,176 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonDownload = new System.Windows.Forms.Button();
+            this.textBoxAddress = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.progressBarDownload = new System.Windows.Forms.ProgressBar();
+            this.textBoxSpeed = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBoxRest = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBoxLoaded = new System.Windows.Forms.TextBox();
+            this.buttonCancel = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // buttonDownload
+            // 
+            this.buttonDownload.Location = new System.Drawing.Point(12, 77);
+            this.buttonDownload.Name = "buttonDownload";
+            this.buttonDownload.Size = new System.Drawing.Size(247, 32);
+            this.buttonDownload.TabIndex = 0;
+            this.buttonDownload.Text = "Загрузить";
+            this.buttonDownload.UseVisualStyleBackColor = true;
+            this.buttonDownload.Click += new System.EventHandler(this.buttonDownload_Click);
+            // 
+            // textBoxAddress
+            // 
+            this.textBoxAddress.Location = new System.Drawing.Point(15, 35);
+            this.textBoxAddress.Name = "textBoxAddress";
+            this.textBoxAddress.Size = new System.Drawing.Size(244, 20);
+            this.textBoxAddress.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(97, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Введите адрес ftp";
+            // 
+            // progressBarDownload
+            // 
+            this.progressBarDownload.Location = new System.Drawing.Point(12, 126);
+            this.progressBarDownload.Name = "progressBarDownload";
+            this.progressBarDownload.Size = new System.Drawing.Size(247, 23);
+            this.progressBarDownload.TabIndex = 3;
+            // 
+            // textBoxSpeed
+            // 
+            this.textBoxSpeed.Enabled = false;
+            this.textBoxSpeed.Location = new System.Drawing.Point(159, 163);
+            this.textBoxSpeed.Name = "textBoxSpeed";
+            this.textBoxSpeed.Size = new System.Drawing.Size(100, 20);
+            this.textBoxSpeed.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 166);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(131, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Скорость передачи Kb/s";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 197);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(94, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Осталось секунд";
+            // 
+            // textBoxRest
+            // 
+            this.textBoxRest.Enabled = false;
+            this.textBoxRest.Location = new System.Drawing.Point(159, 194);
+            this.textBoxRest.Name = "textBoxRest";
+            this.textBoxRest.Size = new System.Drawing.Size(100, 20);
+            this.textBoxRest.TabIndex = 7;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 227);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(88, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Загружено байт";
+            // 
+            // textBoxLoaded
+            // 
+            this.textBoxLoaded.Enabled = false;
+            this.textBoxLoaded.Location = new System.Drawing.Point(159, 224);
+            this.textBoxLoaded.Name = "textBoxLoaded";
+            this.textBoxLoaded.Size = new System.Drawing.Size(100, 20);
+            this.textBoxLoaded.TabIndex = 9;
+            // 
+            // buttonCancel
+            // 
+            this.buttonCancel.Location = new System.Drawing.Point(12, 253);
+            this.buttonCancel.Name = "buttonCancel";
+            this.buttonCancel.Size = new System.Drawing.Size(247, 33);
+            this.buttonCancel.TabIndex = 10;
+            this.buttonCancel.Text = "Отмена";
+            this.buttonCancel.UseVisualStyleBackColor = true;
+            this.buttonCancel.Click += new System.EventHandler(this.buttonCancel_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(273, 299);
+            this.Controls.Add(this.buttonCancel);
+            this.Controls.Add(this.textBoxLoaded);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBoxRest);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBoxSpeed);
+            this.Controls.Add(this.progressBarDownload);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxAddress);
+            this.Controls.Add(this.buttonDownload);
+            this.Name = "Form1";
+            this.Text = "Скачать ftp";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonDownload;
+        private System.Windows.Forms.TextBox textBoxAddress;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ProgressBar progressBarDownload;
+        private System.Windows.Forms.TextBox textBoxSpeed;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBoxRest;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBoxLoaded;
+        private System.Windows.Forms.Button buttonCancel;
+    }
+}
+
---
+++ WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,189 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.Net;
+using System.IO;
+using System.Threading;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        private long rest;
+        private long loaded;
+        private long filesize;
+        private double speed;
+        private bool stop = false;
+        private bool downloadProcessing = false;
+        string uri;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonDownload_Click(object sender, EventArgs e)
+        {
+            if (downloadProcessing)
+            {
+                MessageBox.Show("Закачка уже идет! Подождите или омените!", "Предупреждение", MessageBoxButtons.OK, MessageBoxIcon.Asterisk);
+            }
+            else
+            {
+                uri = textBoxAddress.Text;
+                Thread thread = new Thread(download);
+                thread.Start();
+            }
+        }
+
+        private void download()
+        {
+            stop = false;
+            downloadProcessing = true;
+            string filename = uri.Substring(uri.LastIndexOf('/') + 1);
+            try
+            {
+                FtpWebRequest requestDownload = (FtpWebRequest)WebRequest.Create(uri);
+                FtpWebRequest requestFileSize = (FtpWebRequest)WebRequest.Create(uri);
+
+                requestFileSize.Method = WebRequestMethods.Ftp.GetFileSize;
+                var q = (FtpWebResponse)requestFileSize.GetResponse();
+                filesize = rest = q.ContentLength;
+                loaded = 0;
+                setLoaded(loaded);
+                progressMax((int)filesize);//если размер больше инта
+                progressValue(0);
+
+                requestDownload.Method = WebRequestMethods.Ftp.DownloadFile;
+                FtpWebResponse response = (FtpWebResponse)requestDownload.GetResponse();
+                Stream responseStream = response.GetResponseStream();
+                FileStream file = File.Create(filename);
+                byte[] buffer = new byte[512 * 1024];//оптимальный размер
+                int read;
+                
+                DateTime date = DateTime.Now;
+
+                while (((read = responseStream.Read(buffer, 0, buffer.Length)) > 0) && (!stop))
+                {
+                    file.Write(buffer, 0, read);
+                    TimeSpan diff = DateTime.Now - date;
+                    rest -= read;
+                    loaded += read;
+                    speed = (loaded / diff.TotalSeconds);
+
+                    progressSum(read);
+                    setLoaded(loaded);
+                    setSpeed((int)(speed / 1024));//Kbs
+                    setRest((int)(rest / speed));
+                }
+                file.Close();
+                responseStream.Close();
+                response.Close();
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Ошибка загрузки файла!");
+                downloadProcessing = false;
+                return;
+            }
+
+            downloadProcessing = false;
+
+            if ((filesize != loaded) && (stop))
+            {
+                File.Delete(filename);
+            }
+        }
+
+        private void buttonCancel_Click(object sender, EventArgs e)
+        {
+            stop = true;
+        }
+
+        private void progressSum(int sum)
+        {
+            if (progressBarDownload.InvokeRequired)
+            {
+                progressBarDownload.Invoke(new Action<int>((s) => progressBarDownload.Value += s), sum);
+            }
+            else
+            {
+                progressBarDownload.Value += sum;
+            }
+        }
+
+        private void progressValue(int value)
+        {
+            if (progressBarDownload.InvokeRequired)
+            {
+                progressBarDownload.Invoke(new Action<int>((s) => progressBarDownload.Value = s), value);
+            }
+            else
+            {
+                progressBarDownload.Value = value;
+            }
+        }
+
+        private void progressMax(int max)
+        {
+            if (progressBarDownload.InvokeRequired)
+            {
+                progressBarDownload.Invoke(new Action<int>((s) => progressBarDownload.Maximum = s), max);
+            }
+            else
+            {
+                progressBarDownload.Maximum = max;
+            }
+        }
+
+        private void setSpeed(double speed)
+        {
+            if (textBoxSpeed.InvokeRequired)
+            {
+                textBoxSpeed.Invoke(new Action<string>((s) => textBoxSpeed.Text = s), speed.ToString());
+                textBoxSpeed.Invoke(new Action<string>((s) => textBoxSpeed.Refresh()), speed.ToString());
+            }
+            else
+            {
+                textBoxSpeed.Text = speed.ToString();
+                textBoxSpeed.Refresh();
+            }
+        }
+
+        private void setRest(double rest)
+        {
+            if (textBoxRest.InvokeRequired)
+            {
+                textBoxRest.Invoke(new Action<string>((s) => textBoxRest.Text = s), rest.ToString());
+                textBoxRest.Invoke(new Action<string>((s) => textBoxRest.Refresh()), rest.ToString());
+            }
+            else
+            {
+                textBoxRest.Text = rest.ToString();
+                textBoxRest.Refresh();
+            }
+        }
+
+        private void setLoaded(long loaded)
+        {
+            if (textBoxLoaded.InvokeRequired)
+            {
+                textBoxLoaded.Invoke(new Action<string>((s) => textBoxLoaded.Text = s), loaded.ToString());
+                textBoxLoaded.Invoke(new Action<string>((s) => textBoxLoaded.Refresh()), loaded.ToString());
+            }
+            else
+            {
+                textBoxLoaded.Text = loaded.ToString();
+                textBoxLoaded.Refresh();
+            }
+        }
+    }
+}
